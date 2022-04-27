36850ad1a9854de3dc3d98afda96a6f38bd0e342
unknown <khademim217@gmail.com>
2022-04-27 12:41:22 +0430

    add insert_many

--- sqlite_orm.py
+++ sqlite_orm.py
@@ -7,16 +7,31 @@ class Sqlite:
         self.__cur = self.__con.cursor()
         self.__con.commit()
 
+
     def create_table(self, name:str , *args):
         self.__cur.execute(f"create table '{name}'{args}")
         self.__con.commit()
 
 
-    def insert_one(self, *args):
-        self.__cur.execute(f"insert into test values{args}")
+    def insert_one(self, name,*args):
+        self.__cur.execute(f"insert into {name} values{args}")
         self.__con.commit()
 
 
+    def insert_many(self, name, list):
+        for i in list:
+            self.__cur.execute(f"insert into {name} values{i}")
+        self.__con.commit()
+
+
+
+
+
+
+
+
+
+    
     def close(self):
         self.__con.close()    
 
@@ -25,5 +40,5 @@ class Sqlite:
 
 db = Sqlite('sqlite_orm/sqlite.db')
 #db.create_table('test', 'name', 'age')
-db.insert_one('ali', 20)
+db.insert_many('test', [('hasan' , 15) , ('dwad',156)])
 db.close()
